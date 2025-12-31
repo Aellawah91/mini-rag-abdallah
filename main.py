a8cedb8309fe39a2433f61092d92860ee34c9f3e
Aellawah <abdallahkhaled.60@gmail.com>
2025-12-31 10:01:54 -0800

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
+from routes import base
+
+
 app = FastAPI()
 
-
-
-@app.get("/welcome")
-def welcome():
-    return {"message": "Hello"}
+app.include_router(base.base_router)
